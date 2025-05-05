eb1495b3ff865af0db5b0790385db8f07f4942ce
emon3455 <emon.mhk69@gmail.com>
2025-05-05 10:47:10 +0600

    issues resolve

--- src/constant/baseURL.js
+++ src/constant/baseURL.js
@@ -1,2 +1,3 @@
-export const baseURL = "http://localhost:5000"
+const production = true;
+export const baseURL = production ? "" : "http://localhost:5000"
 export const googleMapsApiKey = "AIzaSyCLw9LSeHpy0kJ7Ujv-CEq74VaeSSOyHNs"
